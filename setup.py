fff691562fc431456ad8f10e58a480ae66fdcf92
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2010-04-11 12:44:42 +0000

    move HISTORY.txt to toplevel CHANGES.txt, and adjust setup.py long_description

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@ import os
 from setuptools import setup, find_packages
 
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 version = '0.2.3'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    read('src', 'collective', 'virtualtreecategories', 'README.txt')
+    )
 
 tests_require = ['collective.testcaselayer']
 
 setup(name='collective.virtualtreecategories',
       version=version,
       description="Virtual tree categories/keywords for Plone",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -23,7 +33,7 @@ setup(name='collective.virtualtreecategories',
       url='http://pypi.python.org/pypi/collective.virtualtreecategories',
       license='GPL',
       packages=find_packages('src', exclude=['ez_setup']),
-      package_dir = {'':'src'},
+      package_dir = {'': 'src'},
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
