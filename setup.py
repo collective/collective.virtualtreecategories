b975a77e696f83cd51c5824e9a20d8740d08f22c
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-09 14:35:31 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.3.2'
+version = '0.3.3'
 
 long_description = (
     read('README.txt')
